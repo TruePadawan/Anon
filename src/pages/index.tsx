f3df40dafbe532e294b787015d9f1079c2695ee3
Hermes <hermeschigoziri@gmail.com>
2023-06-21 21:28:10 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,42 @@
-export default function Home() {
-	return (
-		<main>
-			<h1>Home</h1>
-		</main>
-	);
+import Navbar from "@/components/Navbar/Navbar";
+import { authOptions } from "@/utilities/globals";
+import { GetServerSideProps } from "next";
+import { getServerSession } from "next-auth";
+import Head from "next/head";
+import { ReactElement } from "react";
+
+interface HomeProps {
+	displayName?: string;
 }
+
+const Home = (props: HomeProps) => {
+	const { displayName } = props;
+	return (
+		<>
+			<Navbar displayName={displayName} />
+			<main></main>
+		</>
+	);
+};
+
+Home.getLayout = function getLayout(page: ReactElement) {
+	return (
+		<>
+			<Head>
+				<title key="title">ANON</title>
+			</Head>
+			{page}
+		</>
+	);
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const session = await getServerSession(context.req, context.res, authOptions);
+	return {
+		props: {
+			displayName: session?.user?.name || "",
+		},
+	};
+};
+
+export default Home;
