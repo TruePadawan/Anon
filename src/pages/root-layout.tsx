a809fca56a43b98a435c98910d2c50832ab920b8
Hermes <hermeschigoziri@gmail.com>
2023-06-21 16:23:42 +0100

    Add sign in link

--- src/pages/root-layout.tsx
+++ src/pages/root-layout.tsx
@@ -11,10 +11,15 @@ export default function RootLayout({ children }: RootLayoutProps) {
 			<Head>
 				<title key="title">Anon</title>
 			</Head>
-			<nav className="flex justify-between">
+			<nav className="flex justify-between items-center">
 				<h1 className="font-extrabold text-5xl">
 					<Link href="/">ANON</Link>
 				</h1>
+				<Link
+					href="/sign_in"
+					className="text-xl font-semibold hover:text-accent-color-1">
+					SIGN IN
+				</Link>
 			</nav>
 			{children}
 		</>
