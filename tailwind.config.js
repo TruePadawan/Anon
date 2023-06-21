ae9ba2482f2e82e591e5430f6da46d24bc883c83
Hermes <hermeschigoziri@gmail.com>
2023-06-21 15:40:05 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"primary-color-1": "#111111",
+				"primary-color-2": "#161616",
+				"secondary-color": "#232323",
+				"accent-color-1": "#3e3e3e",
+				"accent-color-2": "#b3b3b3",
+			},
+			boxShadow: {
+				"input-shadow": "inset 0px 2px 2px rgba(69, 69, 69, 0.25)",
+			},
+		},
+	},
+	plugins: [],
+};
