379d0169dcc42076ff20e9c74accf274eaa2580e
Hermes <hermeschigoziri@gmail.com>
2023-06-23 21:07:07 +0100

    Update Navbar.cy.tsx

--- src/components/Navbar/Navbar.cy.tsx
+++ src/components/Navbar/Navbar.cy.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import Navbar from "./Navbar";
+import * as NextRouter from "next/router";
 
 describe("<Navbar />", () => {
+	beforeEach(() => {
+		const push = cy.stub();
+		cy.stub(NextRouter, "useRouter").returns({ push });
+	});
+
 	it("renders application title", () => {
 		cy.mount(<Navbar />);
 		cy.contains("ANON");
 	});
 
-	it("renders a dropdown if displayname is passed", () => {
-		cy.mount(<Navbar displayName="Hermes Chi"/>);
-		cy.get("[data-cy='user-dropdown']");
+	it("renders a dropdown if user object is passed", () => {
+		const user = {
+			displayName: "Hermes Chi",
+			accountName: "hermeschi",
+		};
+		cy.mount(<Navbar user={user} />);
+		cy.get("[data-cy='profile-dropdown']");
 	});
 
-	it("renders a sign in link if no username is passed", () => {
+	it("renders a sign in link if no user is passed", () => {
 		cy.mount(<Navbar />);
 		cy.get("a[href='/sign_in']");
 	});
