0081f0d029e37ffb63d08077032fe1c73a0cafc7
Hermes <hermeschigoziri@gmail.com>
2023-06-23 10:38:57 +0100

    change favicon
    set favicon based on color scheme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,16 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<link rel="icon" href="/favicon.ico" />
+				<link
+					href="/favicon-light.png"
+					rel="icon"
+					media="(prefers-color-scheme: dark)"
+				/>
+				<link
+					href="/favicon-dark.png"
+					rel="icon"
+					media="(prefers-color-scheme: light)"
+				/>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link
