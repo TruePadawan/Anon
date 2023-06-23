a866b5b8c653fec2ca5792004be3d77795968895
Hermes <hermeschigoziri@gmail.com>
2023-06-23 21:08:15 +0100

    Show dropdown when user is signed in
    The dropdown lets user visit their profile or signout

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,47 @@
 import Link from "next/link";
+import ProfileMenu from "../ProfileMenu/ProfileMenu";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/router";
 
 interface NavbarProps {
-	displayName?: string;
+	user?: {
+		displayName: string;
+		accountName: string;
+	};
 	toIndex?: boolean;
 }
 
-export default function Navbar({ displayName, toIndex }: NavbarProps) {
+export default function Navbar({ user, toIndex }: NavbarProps) {
+	const router = useRouter();
+
+	function routeToProfile() {
+		router.push(`/users/${user?.accountName}`);
+	}
+
+	async function handleSignout() {
+		try {
+			const { url } = await signOut({ redirect: false, callbackUrl: "/" });
+			router.push(url);
+		} catch (error) {
+			// this should show error message on a snackbar
+			alert("Problem Signing out");
+			console.error(error);
+		}
+	}
+
 	return (
 		<nav className="flex justify-between items-center">
 			<h1 className="font-extrabold text-5xl">
 				<Link href="/">ANON</Link>
 			</h1>
-			{displayName && <span>{displayName}</span>}
-			{!displayName && (
+			{user && (
+				<ProfileMenu
+					displayName={user.displayName}
+					onProfileMenuItemClicked={routeToProfile}
+					onSignoutMenuItemClicked={handleSignout}
+				/>
+			)}
+			{!user && (
 				<Link
 					href={toIndex ? "/" : "/sign_in"}
 					className="text-xl font-semibold hover:text-accent-color-1">
