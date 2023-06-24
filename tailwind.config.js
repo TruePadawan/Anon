ff203fe8d9a3418ac6e41e5a911ef6a4ffc62425
Hermes <hermeschigoziri@gmail.com>
2023-06-24 04:26:29 +0100

    Add lighter versions of pallete colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,11 @@ module.exports = {
 				"secondary-color": "#232323",
 				"accent-color-1": "#3e3e3e",
 				"accent-color-2": "#b3b3b3",
+				"primary-color-1-l": "#282828",
+				"primary-color-2-l": "#2D2D2D",
+				"secondary-color-l": "#393939",
+				"accent-color-1-l": "#515151",
+				"accent-color-2-l": "#B9B9B9",
 			},
 			boxShadow: {
 				"input-shadow": "inset 0px 2px 2px rgba(69, 69, 69, 0.25)",
