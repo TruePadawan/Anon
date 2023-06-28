a4af89be6cbed3bd92b5fb698f731551c8e4137f
Hermes <hermeschigoziri@gmail.com>
2023-06-28 14:45:30 +0100

    Update UserProfile.ts

--- models/UserProfile.ts
+++ models/UserProfile.ts
@@ -1,7 +1,7 @@
 import { Schema, model, models } from "mongoose";
 import { getRandomColor } from "../helpers/global-helpers";
 
-export interface UserProfile {
+export interface UserProfileProps {
 	avatar_url?: string;
 	display_name: string;
 	account_name: string;
@@ -10,14 +10,14 @@ export interface UserProfile {
 	timestamp: Date;
 }
 
-const UserProfileSchema = new Schema<UserProfile>({
+const UserProfileSchema = new Schema<UserProfileProps>({
 	account_name: { type: String, required: true },
 	display_name: { type: String, required: true },
-	avatar_url: { type: String },
-	bio: { type: String },
+	avatar_url: { type: String, default: "" },
+	bio: { type: String, default: "Hello, I just joined ANON" },
 	color: { type: String, required: true, default: getRandomColor() },
 	timestamp: { type: Date, default: Date.now() },
 });
 
 export default models.UserProfile ||
-	model<UserProfile>("UserProfile", UserProfileSchema);
+	model<UserProfileProps>("UserProfile", UserProfileSchema);
