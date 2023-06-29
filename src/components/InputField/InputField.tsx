c1193b34d594722ad58d22975336eb4588eb9d1c
Hermes <hermeschigoziri@gmail.com>
2023-06-29 18:13:27 +0100

    allow passing ref via prop to underlying input/textarea component

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -3,17 +3,26 @@ import { HTMLInputTypeAttribute } from "react";
 export interface InputFieldProps extends React.HTMLProps<HTMLInputElement> {
 	label: string;
 	type?: HTMLInputTypeAttribute;
+	inputRef?: React.RefObject<HTMLInputElement>;
 	inputElementID: string;
 }
 
 export default function InputField(props: InputFieldProps) {
-	const { label, inputElementID, type = "text", className = "", ...extraProps } = props;
+	const {
+		label,
+		inputElementID,
+		inputRef,
+		type = "text",
+		className = "",
+		...extraProps
+	} = props;
 	return (
 		<div className={`flex flex-col gap-1 ${className}`}>
 			<label htmlFor={inputElementID} className="text-xl font-light">
 				{label}
 			</label>
 			<input
+				ref={inputRef}
 				className="p-0.5 rounded h-8 bg-accent-color-2 shadow-input-shadow text-primary-color-1 font-semibold read-only:text-primary-color-2-l"
 				id={inputElementID}
 				type={type}
--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -3,10 +3,17 @@ import { TextareaAutosize, TextareaAutosizeProps } from "@mui/material";
 export interface TextAreaProps extends TextareaAutosizeProps {
 	label: string;
 	textareaID: string;
+	textareaRef?: React.RefObject<HTMLTextAreaElement>;
 }
 
 export default function TextArea(props: TextAreaProps) {
-	const { label, textareaID, className = "", ...extraProps } = props;
+	const {
+		label,
+		textareaID,
+		textareaRef,
+		className = "",
+		...extraProps
+	} = props;
 
 	return (
 		<div className={`flex flex-col gap-1 ${className}`}>
@@ -14,6 +21,7 @@ export default function TextArea(props: TextAreaProps) {
 				{label}
 			</label>
 			<TextareaAutosize
+				ref={textareaRef}
 				id={textareaID}
 				className="p-0.5 rounded h-8 bg-accent-color-2 shadow-input-shadow text-primary-color-1 font-semibold read-only:text-primary-color-2-l"
 				minRows={4}
