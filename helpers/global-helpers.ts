96ecf0e751ec2154b93288b29acf5e4174b4690b
Hermes <hermeschigoziri@gmail.com>
2023-06-29 08:11:19 +0100

    Update global-helpers.ts

--- helpers/global-helpers.ts
+++ helpers/global-helpers.ts
@@ -1,4 +1,5 @@
 import randomColor from "randomcolor";
+import UserProfile from "../models/UserProfile";
 
 export function getRandomColor() {
 	return randomColor({
@@ -9,4 +10,19 @@ export function getRandomColor() {
 
 export function getRandomInt(max: number) {
 	return Math.floor(Math.random() * max);
+}
+
+export async function getNavbarUserProp(userID: string) {
+	const userProfile = await UserProfile.findById(
+		userID,
+		"display_name account_name"
+	).exec();
+
+	if (userProfile === null) {
+		return null;
+	}
+	return {
+		accountName: userProfile.account_name,
+		displayName: userProfile.display_name,
+	};
 }
