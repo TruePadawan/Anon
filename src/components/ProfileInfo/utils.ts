3e5d1e77add0b3d730d47da9a93efc06bf81f57d
Hermes <hermeschigoziri@gmail.com>
2023-06-30 09:04:16 +0100

    placeholder function for validating account name

--- src/components/ProfileInfo/utils.ts
+++ src/components/ProfileInfo/utils.ts
@@ -1,13 +1,5 @@
-// import dbConnect from "../../../lib/db-connect";
-import UserProfile from "../../../models/UserProfile";
-
 // Account name is valid if it's unique in database
 export async function validateAccountName(value: string) {
 	if (!value) return false;
-
-	const count = await UserProfile.countDocuments({
-		account_name: value,
-	}).exec();
-
-	return count === 0;
+	return false;
 }
