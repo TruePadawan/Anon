1753abcceadaf54f77220581b07c46c8f6d3192d
Hermes <hermeschigoziri@gmail.com>
2023-07-01 21:12:32 +0100

    after profile is updated, page should reload to possibly new account name

--- src/pages/users/[account-name]/index.tsx
+++ src/pages/users/[account-name]/index.tsx
@@ -49,7 +49,7 @@ const Profile = (props: ProfileProps) => {
 					<EditProfileInfo
 						profileData={profile}
 						onCancel={() => setIsEditingProfile(false)}
-						onUpdate={() => router.reload()}
+						onUpdate={() => router.replace("/users")}
 					/>
 				)}
 				{!isEditingProfile && (
