a96d38500d61bf1d461502017999f99146b62d92
Hermes <hermeschigoziri@gmail.com>
2023-07-01 21:07:57 +0100

    Update useInput.ts

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,19 +1,22 @@
 import { useEffect, useState } from "react";
 
-export type InputValidateFnType = (...args: any[]) => Promise<boolean>;
-export type InputDefaultValueType = string | number;
-
+export type InputValue = string | number | undefined;
+type InputValidateFn = (value: InputValue) => Promise<boolean>;
+interface UseInputOptions {
+	defaultValue?: InputValue;
+	transform?: (value: InputValue) => InputValue;
+}
 /**
     `useInput` hook manages the value of an input element.  
     Using debouncing, it validates the input's value when it changes and keeps track
     of its validation status.  
 */
 export default function useInput(
-	validate: InputValidateFnType,
-	defaultValue: InputDefaultValueType = ""
+	validate: InputValidateFn,
+	options: UseInputOptions
 ) {
 	const [isInputValid, setIsInputValid] = useState(false);
-	const [inputValue, setInputValue] = useState(defaultValue);
+	const [inputValue, setInputValue] = useState(options.defaultValue);
 	const [checkingValidity, setCheckingValidity] = useState(false);
 
 	// Apply debouncing to validate input value when it changes
@@ -29,7 +32,11 @@ export default function useInput(
 	}, [inputValue, validate]);
 
 	function changeEventHandler(event: React.ChangeEvent<HTMLInputElement>) {
-		setInputValue(event.target.value);
+		if (options.transform) {
+			setInputValue(options.transform(event.target.value));
+		} else {
+			setInputValue(event.target.value);
+		}
 	}
 
 	return {
