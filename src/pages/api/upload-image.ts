ea944bf16dc5004bbb5ac09aa40f7f765bc9731e
Hermes <hermeschigoziri@gmail.com>
2023-07-05 10:43:48 +0100

    create api route for uploading image

---
+++ src/pages/api/upload-image.ts
@@ -0,0 +1,18 @@
+import { v2 as cloudinary } from "cloudinary";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	if (req.method !== "POST" || !req.body) {
+		res.status(400).json({
+			message: "Should receive a POST request with upload data",
+		});
+	} else {
+		// upload via cloudinary SDK
+		const { file, options } = JSON.parse(req.body);
+		const uploadResponse = await cloudinary.uploader.upload(file, options);
+		res.status(200).json(uploadResponse);
+	}
+}
