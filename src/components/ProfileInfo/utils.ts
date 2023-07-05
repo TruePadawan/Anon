36abdae8f77cd71697f71fe81f5b28922d07faa4
Hermes <hermeschigoziri@gmail.com>
2023-07-05 09:55:26 +0100

    remove moved function

--- src/components/ProfileInfo/utils.ts
+++ src/components/ProfileInfo/utils.ts
@@ -12,17 +12,3 @@ export async function validateAccountName(value: InputValue) {
 
 export const IMG_WIDTH = 150;
 export const IMG_HEIGHT = IMG_WIDTH;
-
-export function validateFileAsImage(file: File) {
-	const errorMessages = [];
-	if (file.size / 1000 >= 1024) {
-		errorMessages.push("Selected file must be less than or equal to 1MB");
-	}
-	if (file.type.split("/").at(0) !== "image") {
-		errorMessages.push("Selected file must be an image");
-	}
-	return {
-		isValid: errorMessages.length === 0,
-		messages: errorMessages,
-	};
-}
