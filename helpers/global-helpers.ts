a6fdad4487e006e61bb78ff26c4cc19cabc84d47
Hermes <hermeschigoziri@gmail.com>
2023-07-05 10:13:29 +0100

    add function to handle image upload

--- helpers/global-helpers.ts
+++ helpers/global-helpers.ts
@@ -28,22 +28,46 @@ export function validateFileAsImage(file: File) {
 	};
 }
 
-export async function generateUploadSignature(uploadOptions: object) {
+export async function generateUploadSignature(uploadParams: object) {
 	const response = await fetch("/api/upload-signature", {
 		method: "POST",
 		body: JSON.stringify({
-			params: uploadOptions,
+			params: uploadParams,
 		}),
 	});
 	const data = await response.json();
 	return data;
 }
 
-export function getBase64(file: File) {
+export function getBase64(file: File): Promise<string | ArrayBuffer | null> {
 	return new Promise((resolve, reject) => {
 		const reader = new FileReader();
 		reader.readAsDataURL(file);
 		reader.onload = () => resolve(reader.result);
 		reader.onerror = (error) => reject(error);
 	});
+}
+
+export async function uploadImage(imageFile: File, uploadParams: FormData) {
+	const { isValid } = validateFileAsImage(imageFile);
+	if (!isValid) {
+		throw new Error("Could not verify that file is an image");
+	}
+	const fileBase64 = await getBase64(imageFile);
+	if (!fileBase64) {
+		throw new Error("Failed to convert file to base64");
+	}
+
+	uploadParams.append("file", fileBase64.toString());
+	const response = await fetch(CLOUDINARY_UPLOAD_URL, {
+		method: "POST",
+		body: uploadParams,
+	});
+
+	if (!response.ok) {
+		throw new Error(response.statusText);
+	}
+
+	const data = await response.json();
+	return data;
 }
