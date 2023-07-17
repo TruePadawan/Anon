998615b32b8fc3eab9b587c5de775e2a9324839e
Hermes <hermeschigoziri@gmail.com>
2023-07-17 09:14:46 +0100

    Create PostEditor component
    PostEditor renders a TipTap editor with the toolbar configured.
    It takes an editor prop which holds the editor object to be passed to the underlying RichTextEditor component

--- src/components/CreatePost/CreatePost.tsx
+++ src/components/CreatePost/CreatePost.tsx
@@ -4,6 +4,7 @@ import { JSONContent, useEditor } from "@tiptap/react";
 import Placeholder from "@tiptap/extension-placeholder";
 import { PostEditorExtensions } from "../../../helpers/global-helpers";
 import { Button } from "@mantine/core";
+import PostEditor from "../PostEditor/PostEditor";
 
 interface CreatePostProps {
 	anonymous?: boolean;
@@ -41,49 +42,7 @@ export default function CreatePost(props: CreatePostProps) {
 			className={`max-w-3xl w-full flex flex-col gap-2 ${
 				props.className || ""
 			}`}>
-			<RichTextEditor editor={editor} spellCheck={false}>
-				<RichTextEditor.Toolbar sticky stickyOffset={60}>
-					<RichTextEditor.ControlsGroup>
-						<RichTextEditor.Bold />
-						<RichTextEditor.Italic />
-						<RichTextEditor.Underline />
-						<RichTextEditor.Strikethrough />
-						<RichTextEditor.ClearFormatting />
-					</RichTextEditor.ControlsGroup>
-
-					<RichTextEditor.ControlsGroup>
-						<RichTextEditor.Code />
-						<RichTextEditor.CodeBlock />
-					</RichTextEditor.ControlsGroup>
-
-					<RichTextEditor.ControlsGroup>
-						<RichTextEditor.H1 />
-						<RichTextEditor.H2 />
-						<RichTextEditor.H3 />
-						<RichTextEditor.H4 />
-					</RichTextEditor.ControlsGroup>
-
-					<RichTextEditor.ControlsGroup>
-						<RichTextEditor.Blockquote />
-						<RichTextEditor.BulletList />
-						<RichTextEditor.OrderedList />
-					</RichTextEditor.ControlsGroup>
-
-					<RichTextEditor.ControlsGroup>
-						<RichTextEditor.Link />
-						<RichTextEditor.Unlink />
-					</RichTextEditor.ControlsGroup>
-
-					<RichTextEditor.ControlsGroup>
-						<RichTextEditor.AlignLeft />
-						<RichTextEditor.AlignCenter />
-						<RichTextEditor.AlignJustify />
-						<RichTextEditor.AlignRight />
-					</RichTextEditor.ControlsGroup>
-				</RichTextEditor.Toolbar>
-
-				<RichTextEditor.Content />
-			</RichTextEditor>
+			<PostEditor editor={editor} />
 			<Button
 				type="button"
 				color="gray"
---
+++ src/components/PostEditor/PostEditor.tsx
@@ -0,0 +1,54 @@
+import { RichTextEditor } from "@mantine/tiptap";
+import { Editor } from "@tiptap/react";
+
+interface PostEditorProps {
+	editor: Editor | null;
+}
+
+export default function PostEditor(props: PostEditorProps) {
+	return (
+		<RichTextEditor editor={props.editor} spellCheck={false}>
+			<RichTextEditor.Toolbar>
+				<RichTextEditor.ControlsGroup>
+					<RichTextEditor.Bold />
+					<RichTextEditor.Italic />
+					<RichTextEditor.Underline />
+					<RichTextEditor.Strikethrough />
+					<RichTextEditor.ClearFormatting />
+				</RichTextEditor.ControlsGroup>
+
+				<RichTextEditor.ControlsGroup>
+					<RichTextEditor.Code />
+					<RichTextEditor.CodeBlock />
+				</RichTextEditor.ControlsGroup>
+
+				<RichTextEditor.ControlsGroup>
+					<RichTextEditor.H1 />
+					<RichTextEditor.H2 />
+					<RichTextEditor.H3 />
+					<RichTextEditor.H4 />
+				</RichTextEditor.ControlsGroup>
+
+				<RichTextEditor.ControlsGroup>
+					<RichTextEditor.Blockquote />
+					<RichTextEditor.BulletList />
+					<RichTextEditor.OrderedList />
+				</RichTextEditor.ControlsGroup>
+
+				<RichTextEditor.ControlsGroup>
+					<RichTextEditor.Link />
+					<RichTextEditor.Unlink />
+				</RichTextEditor.ControlsGroup>
+
+				<RichTextEditor.ControlsGroup>
+					<RichTextEditor.AlignLeft />
+					<RichTextEditor.AlignCenter />
+					<RichTextEditor.AlignJustify />
+					<RichTextEditor.AlignRight />
+				</RichTextEditor.ControlsGroup>
+			</RichTextEditor.Toolbar>
+
+			<RichTextEditor.Content />
+		</RichTextEditor>
+	);
+}
