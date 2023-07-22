5b0639159050a8c0d4762a1a3e79e3d6a709d193
Hermes <hermeschigoziri@gmail.com>
2023-07-22 14:09:02 +0100

    setup basic functionality for posting comment
    create functions and api route to handle posting of comment

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -4,6 +4,8 @@ import CommentEditor from "../Editor/CommentEditor";
 import { useMemo, useState } from "react";
 import CommentItem from "./CommentItem";
 import { UserProfile } from "@prisma/client";
+import { Button } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
 
 interface CommentsProps {
 	commentGroupID: string;
@@ -17,14 +19,63 @@ export default function Comments(props: CommentsProps) {
 	const editor = useEditor({
 		extensions: CommentEditorExtensions,
 	});
-	const [commentIDs, setCommentIDs] = useState([]);
+	const [commentIDs, setCommentIDs] = useState<string[]>([]);
+	const [creatingComment, setCreatingComment] = useState(false);
 	const { commentGroupID, currentUser, initialCommentsCount = 5 } = props;
-
 	// use swr to fetch for comments
 
 	const comments = useMemo(() => {
 		return commentIDs.map((id) => <CommentItem key={id} id={id} />);
 	}, [commentIDs]);
+
+	function getCommentObject() {
+		if (editor === null) {
+			throw new Error("Failed to initialize editor");
+		} else if (props.currentUser === null) {
+			throw new Error("Unsigned user can't make comments");
+		}
+
+		const invalidComment =
+			editor.isEmpty || editor.getText().trim().length === 0;
+
+		if (invalidComment) {
+			throw new Error("A comment can't be empty or just whitespace");
+		}
+		return {
+			commentGroupId: commentGroupID,
+			content: editor.getJSON(),
+			authorId: props.currentUser.id,
+			createdAt: Date.now(),
+		};
+	}
+
+	async function createComment() {
+		setCreatingComment(true);
+		try {
+			const comment = getCommentObject();
+			const response = await fetch("/api/create-comment", {
+				method: "POST",
+				body: JSON.stringify(comment),
+			});
+			const responseBody = await response.json();
+			if (!response.ok) {
+				throw new Error(responseBody.message);
+			}
+			const newCommentID = responseBody.commentID;
+			setCommentIDs((IDs) => [newCommentID, ...IDs]);
+
+			// clear comment editor after comment is created
+			editor?.commands.clearContent();
+		} catch (error: any) {
+			console.error(error);
+			notifications.show({
+				color: "red",
+				title: "Failed to create comment",
+				message: error.message,
+			});
+		}
+		setCreatingComment(false);
+	}
 
 	const commentsCount = commentIDs.length;
 	if (props.showOnlyCommentsCount) {
@@ -36,7 +87,20 @@ export default function Comments(props: CommentsProps) {
 	}
 	return (
 		<div className="w-full flex flex-col gap-2">
-			{props.commentsAllowed && <CommentEditor editor={editor} />}
+			{props.commentsAllowed && (
+				<div>
+					<CommentEditor editor={editor} />
+					<Button
+						type="button"
+						className="mt-1 w-full"
+						size="md"
+						color="gray"
+						onClick={createComment}
+						disabled={creatingComment}>
+						Post comment
+					</Button>
+				</div>
+			)}
 			<ul>{comments}</ul>
 		</div>
 	);
---
+++ src/pages/api/create-comment.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../lib/prisma-client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../lib/auth";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	if (req.method !== "POST" || !req.body) {
+		res.status(400).json({
+			message: "Should receive a POST request with comment data",
+		});
+	} else {
+		const session = await getServerSession(req, res, authOptions);
+		if (!session) {
+			res.status(401).json({ message: "Client not authenticated!" });
+		} else {
+			try {
+				const createdComment = await prisma.comment.create({
+					data: JSON.parse(req.body),
+				});
+				res.status(200).json({
+					commentID: createdComment.id,
+				});
+			} catch (error: any) {
+				res.status(500).json({
+					message: error.message,
+				});
+			}
+		}
+	}
+}
