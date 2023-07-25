569221ee37b7c28bfcb635e64c1e8e6ac2696926
Hermes <hermeschigoziri@gmail.com>
2023-07-25 11:22:47 +0100

    Build CommentItem component
    Component renders a skeleton while fetching comment data

--- src/components/Comments/CommentItem.tsx
+++ src/components/Comments/CommentItem.tsx
@@ -1,7 +1,131 @@
+import { CommentFull } from "@/types/types";
+import {
+	Avatar,
+	Menu,
+	ActionIcon,
+	useMantineTheme,
+	Skeleton,
+} from "@mantine/core";
+import { RichTextEditor } from "@mantine/tiptap";
+import { UserProfile } from "@prisma/client";
+import { IconDots, IconEdit, IconTrash } from "@tabler/icons-react";
+import { Content, useEditor } from "@tiptap/react";
+import moment from "moment";
+import Link from "next/link";
+import useSWR from "swr";
+import { CommentEditorExtensions } from "../../../helpers/global-helpers";
+
 interface CommentItemProps {
 	id: string;
+	currentUser: UserProfile | null;
+	className?: string;
 }
 
-export default function CommentItem({ id }: CommentItemProps) {
-	return <li>comment</li>;
+const fetcher = async (key: string): Promise<CommentFull> => {
+	const response = await fetch(key);
+	if (!response.ok) {
+		const error = await response.json();
+		throw new Error(error.message);
+	}
+	const comments = await response.json();
+	return comments;
+};
+
+export default function CommentItem(props: CommentItemProps) {
+	const theme = useMantineTheme();
+	const {
+		data: commentData,
+		isLoading,
+		error,
+	} = useSWR(`/api/get-comment/${props.id}`, fetcher);
+	const editor = useEditor({
+		editable: false,
+		extensions: CommentEditorExtensions,
+	});
+
+	console.log(1);
+	if (isLoading) {
+		return (
+			<div
+				className="flex flex-col p-1 rounded-md"
+				style={{ backgroundColor: theme.colors.dark[7] }}>
+				<div className="flex gap-1.5 py-1 px-1">
+					<Skeleton circle height={40} />
+					<div className="flex grow flex-col gap-1.5">
+						<Skeleton height={25} />
+						<Skeleton height={80} />
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	if (error || !commentData) {
+		return (
+			<li>
+				<p className="text-center text-sm">Comment no longer exists</p>
+			</li>
+		);
+	}
+
+	// set editor content after comment data is fetched
+	if (editor?.isEmpty) {
+		editor.commands.setContent(commentData.content as Content);
+	}
+
+	const { author } = commentData;
+	const currentUserIsAuthor = props.currentUser?.id === author.id;
+	const creationDate = moment(commentData.createdAt).fromNow(true);
+	return (
+		<li
+			className="flex flex-col p-1 rounded-md"
+			style={{ backgroundColor: theme.colors.dark[7] }}>
+			<div className="flex gap-1.5 py-1 px-1">
+				<Avatar
+					variant="filled"
+					radius="xl"
+					color={author.color}
+					src={author.avatarUrl}
+				/>
+				<div className="flex grow flex-col gap-1.5">
+					<div className="flex justify-between">
+						<div className="flex items-center gap-0.5">
+							<Link href={`/users/${author.accountName}`}>
+								<span className="font-semibold">{author.displayName}</span>
+							</Link>
+							<Link href={`/users/${author.accountName}`}>
+								<span className="text-gray-500 text-sm">{`@${author.accountName}`}</span>
+							</Link>
+							<span>·</span>
+							<span className="text-gray-500 text-sm">{creationDate}</span>
+						</div>
+						{currentUserIsAuthor && (
+							<Menu>
+								<Menu.Target>
+									<ActionIcon>
+										<IconDots />
+									</ActionIcon>
+								</Menu.Target>
+								<Menu.Dropdown>
+									<Menu.Label>Manage</Menu.Label>
+									<Menu.Item icon={<IconEdit size={16} />}>Edit</Menu.Item>
+									<Menu.Item color="red" icon={<IconTrash size={16} />}>
+										Delete
+									</Menu.Item>
+								</Menu.Dropdown>
+							</Menu>
+						)}
+					</div>
+					<RichTextEditor
+						editor={editor}
+						styles={{
+							root: { border: "none" },
+							content: { "& .ProseMirror": { padding: "0" } },
+						}}>
+						<RichTextEditor.Content />
+					</RichTextEditor>
+				</div>
+			</div>
+		</li>
+	);
 }
