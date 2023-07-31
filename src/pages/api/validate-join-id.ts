77c24befd49796027b878346678828ca325e3011
Hermes <hermeschigoziri@gmail.com>
2023-07-31 17:27:00 +0100

    Create api route for verifying if a join id is valid

---
+++ src/pages/api/validate-join-id.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../lib/prisma-client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../lib/auth";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const session = await getServerSession(req, res, authOptions);
+	if (!session) {
+		res.status(401).json({ message: "Client not authenticated!?" });
+	} else {
+		const id = JSON.parse(req.body).id;
+		if (!id) {
+			res.status(400).json({ messsage: "Join ID not provided" });
+		} else {
+			const groupSettings = await prisma.groupSettings.findUnique({
+				where: {
+					groupJoinId: id,
+				},
+				include: {
+					group: true,
+				},
+			});
+			if (groupSettings) {
+				res.status(200).json(groupSettings.group);
+			} else {
+				res.status(404).json({ message: `Group with ID ${id} not found` });
+			}
+		}
+	}
+}
