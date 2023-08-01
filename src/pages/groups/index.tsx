cfcdde841eb1abff9d199702b929449d0f23f413
Hermes <hermeschigoziri@gmail.com>
2023-08-01 17:54:23 +0100

    Create search bar and navlink button
    The search bar will be used to search for groups while the navlink routes the user to the page for creating a group

--- src/pages/groups/index.tsx
+++ src/pages/groups/index.tsx
@@ -4,17 +4,49 @@ import { getServerSession } from "next-auth";
 import { UserProfile } from "@prisma/client";
 import { authOptions } from "../../../lib/auth";
 import { prisma } from "../../../lib/prisma-client";
+import { Button, TextInput, useMantineTheme } from "@mantine/core";
+import Link from "next/link";
+import { IconPlus } from "@tabler/icons-react";
 
 interface PageProps {
 	user: UserProfile | null;
 }
 
 const GroupsPage = ({ user }: PageProps) => {
+	const theme = useMantineTheme();
+	function formSubmitHandler(event: React.FormEvent) {
+		event.preventDefault();
+	}
+
 	return (
 		<>
 			<Navbar user={user} />
-			<main className="grow flex flex-col gap-3 items-center">
-				<h2>GROUPS PAGE</h2>
+			<main className="grow flex flex-col">
+				<div className="flex justify-between">
+					<form
+						onSubmit={formSubmitHandler}
+						className="flex gap-1 max-w-xs w-full">
+						<TextInput
+							className="grow"
+							placeholder="Group name"
+							aria-label="group name"
+						/>
+						<Button
+							type="submit"
+							color="gray"
+							sx={{ background: theme.colors.gray[8], color: "white" }}>
+							Search
+						</Button>
+					</form>
+					<Button
+						color="gray"
+						sx={{ background: theme.colors.gray[8], color: "white" }}
+						variant="light"
+						component={Link}
+						href="/groups/new">
+						Create a group
+					</Button>
+				</div>
 			</main>
 		</>
 	);
