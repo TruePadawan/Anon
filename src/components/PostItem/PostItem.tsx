a04b9cd632cc1f2aa5df2aaece4cfc69529a15c9
Hermes <hermeschigoziri@gmail.com>
2023-08-02 15:07:41 +0100

    minor change

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -58,6 +58,20 @@ export default function PostItem(props: PostItemProps) {
 	);
 	const editorContentRef = useRef(editor?.getHTML());
 
+	if (postDeleted) {
+		return (
+			<li className={`flex flex-col gap-4 ${props.className || ""}`}>
+				<Alert
+					className="grow"
+					icon={<IconCheck size="1rem" />}
+					color="dark"
+					variant="filled">
+					Post deleted successfully
+				</Alert>
+			</li>
+		);
+	}
+
 	function startEditMode() {
 		editorContentRef.current = editor?.getHTML();
 		editor?.setEditable(true);
@@ -125,150 +139,133 @@ export default function PostItem(props: PostItemProps) {
 	const currentUserIsAuthor = currentUser?.id === author.id;
 	return (
 		<li className={`flex flex-col gap-4 ${props.className || ""}`}>
-			{postDeleted && (
-				<Alert
-					className="grow"
-					icon={<IconCheck size="1rem" />}
-					color="dark"
-					variant="filled">
-					Post deleted successfully
-				</Alert>
-			)}
-			{!postDeleted && (
-				<>
-					<div
-						className="flex flex-col p-1 rounded-md"
-						style={{ backgroundColor: theme.colors.dark[7] }}>
-						<div className="flex gap-1.5 py-1 px-1">
-							<Avatar
-								variant="filled"
-								radius="xl"
-								color={author.color}
-								src={author.avatarUrl}
-							/>
-							<div className="flex grow flex-col gap-1.5">
-								<div className="flex justify-between">
-									<div className="flex items-center gap-0.5">
-										<Link href={`/users/${author.accountName}`}>
-											<span className="font-semibold">
-												{author.displayName}
-											</span>
-										</Link>
-										<Link href={`/users/${author.accountName}`}>
-											<span className="text-gray-500 text-sm">{`@${author.accountName}`}</span>
-										</Link>
-										<span>·</span>
-										<span className="text-gray-500 text-sm">
-											{creationDate}
-										</span>
-									</div>
-									<Menu>
-										<Menu.Target>
-											<ActionIcon>
-												<IconDots />
-											</ActionIcon>
-										</Menu.Target>
-										<Menu.Dropdown>
-											{!props.full && (
-												<>
-													<Menu.Label>General</Menu.Label>
-													<Menu.Item
-														component={Link}
-														icon={<IconArrowsMaximize size={16} />}
-														href={`/posts/${postData.id}`}>
-														View full post
-													</Menu.Item>
-												</>
-											)}
-											{currentUserIsAuthor && (
-												<>
-													<Menu.Label>Manage</Menu.Label>
-													<Menu.Item
-														icon={<IconMessageCircle size={16} />}
-														onClick={toggleComments}
-														disabled={inEditMode || isUpdatingPost}>
-														{commentsAllowed
-															? "Disable comments"
-															: "Enable comments"}
-													</Menu.Item>
-													<Menu.Item
-														icon={<IconEdit size={16} />}
-														onClick={startEditMode}
-														disabled={inEditMode || isUpdatingPost}>
-														Edit
-													</Menu.Item>
-													<Menu.Item
-														color="red"
-														icon={<IconTrash size={16} />}
-														onClick={openConfirmDeleteModal}
-														disabled={inEditMode || isUpdatingPost}>
-														Delete
-													</Menu.Item>
-												</>
-											)}
-										</Menu.Dropdown>
-									</Menu>
-									{currentUserIsAuthor && (
-										<Modal
-											opened={confirmDeleteModalOpened}
-											onClose={closeConfirmDeleteModal}
-											title="Confirm Action"
-											centered>
-											<div className="flex flex-col gap-1.5">
-												<p>Are you sure you want to delete this post?</p>
-												<div className="flex flex-col gap-1">
-													<Button color="green" onClick={deletePost}>
-														Yes
-													</Button>
-													<Button color="red" onClick={closeConfirmDeleteModal}>
-														No
-													</Button>
-												</div>
-											</div>
-										</Modal>
-									)}
-								</div>
-								{inEditMode && (
-									<UpdatePost
-										editor={editor as Editor}
-										postID={postData.id}
-										authorId={postData.authorId}
-										isUpdating={isUpdatingPost}
-										setIsUpdatingState={setIsUpdatingPost}
-										onUpdate={stopEditMode}
-										cancelUpdate={cancelEditMode}
-									/>
-								)}
-								{!inEditMode && (
-									<RichTextEditor
-										editor={editor}
-										styles={{
-											root: { border: "none" },
-											content: { "& .ProseMirror": { padding: "0" } },
-										}}>
-										<RichTextEditor.Content />
-									</RichTextEditor>
-								)}
+			<div
+				className="flex flex-col p-1 rounded-md"
+				style={{ backgroundColor: theme.colors.dark[7] }}>
+				<div className="flex gap-1.5 py-1 px-1">
+					<Avatar
+						variant="filled"
+						radius="xl"
+						color={author.color}
+						src={author.avatarUrl}
+					/>
+					<div className="flex grow flex-col gap-1.5">
+						<div className="flex justify-between">
+							<div className="flex items-center gap-0.5">
+								<Link href={`/users/${author.accountName}`}>
+									<span className="font-semibold">{author.displayName}</span>
+								</Link>
+								<Link href={`/users/${author.accountName}`}>
+									<span className="text-gray-500 text-sm">{`@${author.accountName}`}</span>
+								</Link>
+								<span>·</span>
+								<span className="text-gray-500 text-sm">{creationDate}</span>
 							</div>
+							<Menu>
+								<Menu.Target>
+									<ActionIcon>
+										<IconDots />
+									</ActionIcon>
+								</Menu.Target>
+								<Menu.Dropdown>
+									{!props.full && (
+										<>
+											<Menu.Label>General</Menu.Label>
+											<Menu.Item
+												component={Link}
+												icon={<IconArrowsMaximize size={16} />}
+												href={`/posts/${postData.id}`}>
+												View full post
+											</Menu.Item>
+										</>
+									)}
+									{currentUserIsAuthor && (
+										<>
+											<Menu.Label>Manage</Menu.Label>
+											<Menu.Item
+												icon={<IconMessageCircle size={16} />}
+												onClick={toggleComments}
+												disabled={inEditMode || isUpdatingPost}>
+												{commentsAllowed
+													? "Disable comments"
+													: "Enable comments"}
+											</Menu.Item>
+											<Menu.Item
+												icon={<IconEdit size={16} />}
+												onClick={startEditMode}
+												disabled={inEditMode || isUpdatingPost}>
+												Edit
+											</Menu.Item>
+											<Menu.Item
+												color="red"
+												icon={<IconTrash size={16} />}
+												onClick={openConfirmDeleteModal}
+												disabled={inEditMode || isUpdatingPost}>
+												Delete
+											</Menu.Item>
+										</>
+									)}
+								</Menu.Dropdown>
+							</Menu>
+							{currentUserIsAuthor && (
+								<Modal
+									opened={confirmDeleteModalOpened}
+									onClose={closeConfirmDeleteModal}
+									title="Confirm Action"
+									centered>
+									<div className="flex flex-col gap-1.5">
+										<p>Are you sure you want to delete this post?</p>
+										<div className="flex flex-col gap-1">
+											<Button color="green" onClick={deletePost}>
+												Yes
+											</Button>
+											<Button color="red" onClick={closeConfirmDeleteModal}>
+												No
+											</Button>
+										</div>
+									</div>
+								</Modal>
+							)}
 						</div>
-						{!props.full && (
-							<Comments
-								commentGroupID={postData.id}
-								commentsAllowed={commentsAllowed || currentUserIsAuthor}
-								showOnlyCommentsCount={!props.full}
+						{inEditMode && (
+							<UpdatePost
+								editor={editor as Editor}
+								postID={postData.id}
+								authorId={postData.authorId}
+								isUpdating={isUpdatingPost}
+								setIsUpdatingState={setIsUpdatingPost}
+								onUpdate={stopEditMode}
+								cancelUpdate={cancelEditMode}
 							/>
 						)}
+						{!inEditMode && (
+							<RichTextEditor
+								editor={editor}
+								styles={{
+									root: { border: "none" },
+									content: { "& .ProseMirror": { padding: "0" } },
+								}}>
+								<RichTextEditor.Content />
+							</RichTextEditor>
+						)}
 					</div>
-					{props.full && (
-						<>
-							<Divider label="Comments" labelPosition="center" />
-							<Comments
-								commentGroupID={postData.id}
-								commentsAllowed={commentsAllowed || currentUserIsAuthor}
-								showOnlyCommentsCount={!props.full}
-							/>
-						</>
-					)}
+				</div>
+				{!props.full && (
+					<Comments
+						commentGroupID={postData.id}
+						commentsAllowed={commentsAllowed || currentUserIsAuthor}
+						showOnlyCommentsCount={!props.full}
+					/>
+				)}
+			</div>
+			{props.full && (
+				<>
+					<Divider label="Comments" labelPosition="center" />
+					<Comments
+						commentGroupID={postData.id}
+						commentsAllowed={commentsAllowed || currentUserIsAuthor}
+						showOnlyCommentsCount={!props.full}
+					/>
 				</>
 			)}
 		</li>
