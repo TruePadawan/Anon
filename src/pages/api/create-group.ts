22a0a65952abb68990baf70e8bff320ba57c87ab
Hermes <hermeschigoziri@gmail.com>
2023-08-03 17:23:07 +0100

    Create api route for creating a group

---
+++ src/pages/api/create-group.ts
@@ -0,0 +1,59 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../lib/prisma-client";
+
+export interface CreateGroupApiReqBody {
+	groupData: {
+		adminId: string;
+		name: string;
+		desc?: string;
+	};
+	settingsData: {
+		isAnonymous: boolean;
+		autoMemberApproval: boolean;
+		autoPostApproval: boolean;
+	};
+}
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	// use a transaction to create a group and it's settings in the db
+	if (req.method !== "POST" || !req.body) {
+		res.status(400).json({
+			message:
+				"Invalid request, should be a POST request with group+group settings data in the request body",
+		});
+	} else {
+		const { groupData, settingsData }: CreateGroupApiReqBody = JSON.parse(
+			req.body
+		);
+		try {
+			const settings = await prisma.groupSettings.create({
+				data: {
+					isAnonymous: settingsData.isAnonymous,
+					autoMemberApproval: settingsData.autoMemberApproval,
+					autoPostApproval: settingsData.autoPostApproval,
+					Group: {
+						create: {
+							adminId: groupData.adminId,
+							name: groupData.name,
+							desc: groupData.desc,
+							createdAt: Date.now(),
+						},
+					},
+				},
+				include: {
+					Group: true,
+				},
+			});
+			res
+				.status(200)
+				.json({ message: "Group created successfully", group: settings.Group });
+		} catch (error: any) {
+			console.error(error);
+			res
+				.status(500)
+				.json({ message: `Failed to create group: ${error.message}` });
+		}
+	}
+}
