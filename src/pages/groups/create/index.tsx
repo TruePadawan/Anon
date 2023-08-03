b859d38359b694fcac3e38a9ecc9a00eb8da544b
Hermes <hermeschigoziri@gmail.com>
2023-08-03 17:24:14 +0100

    Setup functionality for creating a group

--- src/pages/groups/create/index.tsx
+++ src/pages/groups/create/index.tsx
@@ -1,14 +1,19 @@
 import Navbar from "@/components/Navbar/Navbar";
 import useInput from "@/hooks/useInput";
+import useUser from "@/hooks/useUser";
+import { CreateGroupApiReqBody } from "@/pages/api/create-group";
 import { Button, Group, Radio, TextInput, Textarea } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
 import { Group as GroupModel } from "@prisma/client";
+import { useRouter } from "next/router";
+import { useRef, useState } from "react";
 
 interface fetchResponse {
 	group: GroupModel | null;
 }
 async function validateGroupName(groupName: string) {
 	if (groupName === "") return false;
-	
+
 	const res = await fetch("/api/get-group", {
 		method: "POST",
 		body: JSON.stringify({
@@ -24,8 +29,53 @@ async function validateGroupName(groupName: string) {
 	return group === null;
 }
 
+type RadioValue = "true" | "false" | string;
 export default function CreateGroupPage() {
+	const { user } = useUser();
 	const groupNameInput = useInput(validateGroupName);
+	const descRef = useRef<HTMLTextAreaElement>(null);
+	const [groupIsAnonymous, setGroupIsAnonymous] = useState<RadioValue>("false");
+	const [autoApprovePosts, setAutoApprovePosts] = useState<RadioValue>("true");
+	const [autoApproveMembers, setAutoApproveMembers] =
+		useState<RadioValue>("true");
+	const router = useRouter();
+
+	async function submitHandler(event: React.FormEvent) {
+		event.preventDefault();
+		if (!user) return;
+
+		const payload: CreateGroupApiReqBody = {
+			groupData: {
+				adminId: user.id,
+				name: groupNameInput.inputValue,
+				desc: descRef.current?.value,
+			},
+			settingsData: {
+				isAnonymous: groupIsAnonymous === "true",
+				autoMemberApproval: autoApproveMembers === "true",
+				autoPostApproval: autoApprovePosts === "true",
+			},
+		};
+		const res = await fetch("/api/create-group", {
+			method: "POST",
+			body: JSON.stringify(payload),
+		});
+		if (!res.ok) {
+			const { message } = await res.json();
+			notifications.show({
+				color: "red",
+				title: "Failed to complete action",
+				message,
+			});
+		} else {
+			const { group }: { group: GroupModel } = await res.json();
+			notifications.show({
+				color: "green",
+				message: `Group '${group.name} created successfully`,
+			});
+			router.push("/groups");
+		}
+	}
 
 	const inputIsInvalid =
 		groupNameInput.inputWasTouched &&
@@ -43,6 +93,7 @@ export default function CreateGroupPage() {
 			<Navbar />
 			<main className="flex justify-center">
 				<form
+					onSubmit={submitHandler}
 					aria-labelledby="form-header"
 					className="max-w-lg w-full flex flex-col gap-4">
 					<h2 id="form-header" className="text-4xl text-center">
@@ -65,13 +116,15 @@ export default function CreateGroupPage() {
 							placeholder="This group is for humans currently inhabiting EARTH_1"
 							size="md"
 							minRows={2}
+							ref={descRef}
 							autosize
 						/>
 						<Radio.Group
 							name="group-type"
 							label="Create anonymous group?"
 							description="The profiles of all members are hidden from each other except the admin"
-							defaultValue="false"
+							value={groupIsAnonymous}
+							onChange={setGroupIsAnonymous}
 							size="md"
 							withAsterisk>
 							<Group mt="xs">
@@ -83,7 +136,8 @@ export default function CreateGroupPage() {
 							name="auto-member-approval"
 							label="Automatically approve join request?"
 							description="New members won't need admin approval to join"
-							defaultValue="true"
+							value={autoApproveMembers}
+							onChange={setAutoApproveMembers}
 							size="md"
 							withAsterisk>
 							<Group mt="xs">
@@ -95,7 +149,8 @@ export default function CreateGroupPage() {
 							name="auto-post-approval"
 							label="Automatically approve posts?"
 							description="Posts will be submitted without needing admin approval"
-							defaultValue="true"
+							value={autoApprovePosts}
+							onChange={setAutoApprovePosts}
 							size="md"
 							withAsterisk>
 							<Group mt="xs">
