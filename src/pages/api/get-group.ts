5fb6cddfe14ed47269fd60d0537d2410527f7072
Hermes <hermeschigoziri@gmail.com>
2023-08-03 12:02:01 +0100

    Create api route for getting a group document

---
+++ src/pages/api/get-group.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../lib/prisma-client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../lib/auth";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const session = await getServerSession(req, res, authOptions);
+	if (!session) {
+		res.status(401).json({ message: "Client not authenticated!?" });
+	} else {
+		try {
+			const { name } = JSON.parse(req.body);
+			const group = await prisma.group.findUnique({
+				where: {
+					name,
+				},
+			});
+			res.status(200).json({ group });
+		} catch (error: any) {
+			console.error(error);
+			res.status(500).json({
+				message: error.message,
+			});
+		}
+	}
+}
