6df169a801be46899f73d9123d777b67b0d8b9bc
Hermes <hermeschigoziri@gmail.com>
2023-08-08 16:55:33 +0100

    separate out function for transforming account name
    separate out the transform function used when updating the account name input in useInput hook

--- src/components/ProfileInfo/utils.ts
+++ src/components/ProfileInfo/utils.ts
@@ -15,5 +15,9 @@ export const validateAccountName: Validator = {
 	},
 };
 
+export function parseAccountName(accountName: string) {
+	return accountName?.toString().replaceAll(" ", "");
+}
+
 export const IMG_WIDTH = 150;
 export const IMG_HEIGHT = IMG_WIDTH;
