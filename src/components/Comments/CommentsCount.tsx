59c4dbecf4d125e4a02abb5a5f73e2eeda9b916b
Hermes <hermeschigoziri@gmail.com>
2023-08-20 17:20:56 +0100

    Create ReplyCount component
    ReplyCount like CommentsCount shows the number of replies a comment has

--- src/components/Comments/CommentsCount.tsx
+++ src/components/Comments/CommentsCount.tsx
@@ -1,12 +1,8 @@
 import CommentsAPI from "@/lib/api/CommentsAPI";
 import useSWR from "swr";
 
-interface CommentsCountProps {
-	commentGroupId: string;
-}
-
 export default function CommentsCount({ commentGroupId }: CommentsCountProps) {
-	const { data: count, isLoading } = useSWR(commentGroupId, fetcher);
+	const { data: count, isLoading } = useSWR(commentGroupId, fetchCommentCount);
 	return (
 		<p className="w-full text-center font-semibold">
 			{isLoading ? "0 comments" : `${count} comment${count === 1 ? "" : "s"}`}
@@ -14,7 +10,31 @@ export default function CommentsCount({ commentGroupId }: CommentsCountProps) {
 	);
 }
 
-const fetcher = async (groupId: string): Promise<number> => {
+export function ReplyCount({ commentId }: ReplyCountProps) {
+	const { data: count, isLoading } = useSWR(commentId, fetchReplyCount);
+	return (
+		<p className="w-full text-center text-sm">
+			{isLoading
+				? "0 replies"
+				: `${count} ${count === 1 ? "reply" : "replies"}`}
+		</p>
+	);
+}
+
+async function fetchCommentCount(groupId: string) {
 	const count = await CommentsAPI.count(groupId);
 	return count;
-};
+}
+
+async function fetchReplyCount(commentId: string) {
+	const count = await CommentsAPI.replyCount(commentId);
+	return count;
+}
+
+interface CommentsCountProps {
+	commentGroupId: string;
+}
+
+interface ReplyCountProps {
+	commentId: string;
+}
