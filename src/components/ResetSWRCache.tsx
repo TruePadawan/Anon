b7f4051033c494cf7e4ba70e129b045b6a3f9ee5
Hermes <hermeschigoziri@gmail.com>
2023-09-06 17:21:00 +0100

    Create ResetSWRCache.tsx
    This component resets swr cache, it is used when testing components

---
+++ src/components/ResetSWRCache.tsx
@@ -0,0 +1,11 @@
+import { SWRConfig } from "swr";
+
+interface ResetSWRCacheProps {
+	children: React.ReactNode;
+}
+
+export default function ResetSWRCache({ children }: ResetSWRCacheProps) {
+	return (
+		<SWRConfig value={{ provider: () => new Map() }}>{children}</SWRConfig>
+	);
+}
