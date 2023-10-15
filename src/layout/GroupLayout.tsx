c641e0f1f6bbd807eb7e80a8257d489daf815bae
Chisom Hermes Chigoziri <hermeschigoziri@gmail.com>
2023-10-15 16:25:07 +0100

    Update GroupLayout.tsx

--- src/layout/GroupLayout.tsx
+++ src/layout/GroupLayout.tsx
@@ -1,4 +1,5 @@
-import { ActionIcon, Menu, Tabs } from "@mantine/core";
+import GroupsAPI from "@/lib/api/GroupsAPI";
+import { ActionIcon, Loader, Menu, Tabs } from "@mantine/core";
 import { Group } from "@prisma/client";
 import {
 	IconSettings,
@@ -10,6 +11,7 @@ import {
 } from "@tabler/icons-react";
 import { Montserrat } from "next/font/google";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
 interface GroupLayoutProps {
 	children: React.ReactNode;
@@ -25,6 +27,27 @@ const montserrat = Montserrat({
 export default function GroupLayout(props: GroupLayoutProps) {
 	const router = useRouter();
 	const { groupData } = props;
+	const {
+		data: asideData,
+		error,
+		isLoading,
+	} = useSWR(groupData ? groupData.id : null, async (groupId) => {
+		const group = await GroupsAPI.getOne({
+			where: {
+				id: groupId,
+			},
+			select: {
+				desc: true,
+				admin: {
+					select: {
+						accountName: true,
+					},
+				},
+				_count: { select: { groupMembers: true } },
+			},
+		});
+		return group;
+	});
 
 	if (!groupData) {
 		return (
@@ -39,6 +62,7 @@ export default function GroupLayout(props: GroupLayoutProps) {
 		const groupId = groupData?.id ?? "no-id";
 		router.push(`/groups/${groupId}/${tabValue}`);
 	}
+
 	return (
 		<main className="grow flex flex-col">
 			<div className="flex justify-between items-center px-4 py-2 bg-primary-color-2">
@@ -81,7 +105,54 @@ export default function GroupLayout(props: GroupLayoutProps) {
 						Chatroom
 					</Tabs.Tab>
 				</Tabs.List>
-				<Tabs.Panel value={props.tabValue}>{props.children}</Tabs.Panel>
+				<Tabs.Panel value={props.tabValue}>
+					<div className="mt-1.5 flex">
+						<div className="grow">{props.children}</div>
+						<aside className="flex flex-col gap-y-1 items-stretch max-w-xl w-full">
+							{asideData && (
+								<ul className="bg-secondary-color p-1.5 flex flex-col gap-y-1.5 rounded-md">
+									<li className="flex flex-col">
+										<span className="font-semibold text-base text-white">
+											Admin
+										</span>
+										<span className="text-sm">
+											{asideData.admin.accountName}
+										</span>
+									</li>
+									<li className="flex flex-col">
+										<span className="font-semibold text-base text-white">
+											Description
+										</span>
+										<p className="text-sm">
+											{asideData.desc || "No description."}
+										</p>
+									</li>
+									<li className="flex flex-col">
+										<span className="font-semibold text-base text-white">
+											Member count
+										</span>
+										<span className="text-sm">
+											{asideData._count.groupMembers}
+										</span>
+									</li>
+								</ul>
+							)}
+							{isLoading && (
+								<div className="bg-secondary-color h-48 rounded-md flex justify-center items-center">
+									<Loader variant="bars" size="md" color="dark" />
+								</div>
+							)}
+							{error && (
+								<div className="bg-secondary-color h-40 text-center rounded-md">
+									<p className="text-white font-semibold text-lg mt-6">
+										Failed to load data
+									</p>
+									<p className="text-sm">{error.message}</p>
+								</div>
+							)}
+						</aside>
+					</div>
+				</Tabs.Panel>
 			</Tabs>
 		</main>
 	);
