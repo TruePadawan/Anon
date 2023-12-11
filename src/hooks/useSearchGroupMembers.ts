4797929f06e63f8e2c924ac5d3499507d1656416
Chisom Hermes Chigoziri <hermeschigoziri@gmail.com>
2023-12-11 08:46:57 +0100

    Update useSearchGroupMembers.ts
    The hook should return an object containing an array of group members and a function for triggering searches

--- src/hooks/useSearchGroupMembers.ts
+++ src/hooks/useSearchGroupMembers.ts
@@ -5,15 +5,33 @@ import { useState } from "react";
  * React hook for searching for group members
  */
 export default function useSearchGroupMembers(
-	groupId: string,
-	accountNameOrDisplayName?: string
-) {
+	groupId?: string
+): useSearchGroupMembersReturnType {
 	const [members, setMembers] = useState<Member[]>([]);
-	return members;
+
+	async function search(accountNameOrDisplayName: string) {
+		if (!groupId) {
+			setMembers([]);
+		} else {
+			// main logic
+		}
+	}
+
+	// return an empty array as members if groupId is not defined
+	return {
+		members,
+		search,
+	};
 }
 
+// custom types
 const groupMemberWithProfile =
 	Prisma.validator<Prisma.GroupMemberDefaultArgs>()({
 		include: { user: true },
 	});
 type Member = Prisma.GroupMemberGetPayload<typeof groupMemberWithProfile>;
+
+interface useSearchGroupMembersReturnType {
+	members: Member[];
+	search: (accountNameOrDisplayName: string) => void;
+}
