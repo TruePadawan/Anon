06c2ce70056c57a7b3dd9af08453a5c0a4b1aea4
Chisom Hermes Chigoziri <hermeschigoziri@gmail.com>
2023-12-21 14:17:42 +0100

    create api routes for group related actions

---
+++ src/pages/api/delete-group-post.ts
@@ -0,0 +1,76 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/lib/prisma-client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { DELETED_POST_CONTENT } from "@/helpers/global_vars";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	if (req.method !== "POST" || !req.body) {
+		return res.status(400).json({
+			message: "Should receive a POST request with a body",
+		});
+	}
+	const session = await getServerSession(req, res, authOptions);
+	if (!session) {
+		return res.status(401).json({ message: "Client not authenticated!?" });
+    }
+    
+	const { id }: Payload = JSON.parse(req.body);
+	const count = await prisma.groupPost.count({
+		where: {
+			id,
+			author: {
+				user: {
+					id: session.user.id,
+				},
+			},
+		},
+	});
+	// the action is authorized if the user is the author of the post
+	const isAuthorized = count === 1;
+	if (isAuthorized) {
+		const commentCount = await prisma.comment.count({
+			where: {
+				groupPost: {
+					id,
+				},
+			},
+		});
+		try {
+			// delete the post from database only if it has no comments, else use reddit's style of handling 'deleted' posts
+			if (commentCount === 0) {
+				await prisma.groupPost.delete({
+					where: {
+						id,
+					},
+				});
+			} else {
+				await prisma.groupPost.update({
+					where: {
+						id,
+					},
+					data: {
+						content: DELETED_POST_CONTENT,
+						isDeleted: true,
+					},
+				});
+			}
+			res.status(200).json({ message: "Post deleted successfully" });
+		} catch (error: any) {
+			res.status(500).json({
+				message: error.message,
+			});
+		}
+	} else {
+		res.status(403).json({
+			message: "Current user is not the author",
+		});
+	}
+}
+
+interface Payload {
+	id: string;
+}
---
+++ src/pages/api/get-group-post/[postId].ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/lib/prisma-client";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const postId = req.query.postId as string | undefined;
+	if (req.method !== "GET" || !postId) {
+		res.status(400).json({
+			message: "Invalid GET request",
+		});
+	} else {
+		try {
+			const post = await prisma.groupPost.findFirstOrThrow({
+				where: {
+					id: postId,
+				},
+				include: {
+					author: true,
+				},
+			});
+			res.status(200).json(post);
+		} catch (error: any) {
+			res.status(500).json({
+				message: error.message,
+			});
+		}
+	}
+}
