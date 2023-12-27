cb854076e661cbb846dd70c4ae0307af85ed8380
Chisom Hermes Chigoziri <hermeschigoziri@gmail.com>
2023-12-27 09:30:40 +0100

    Update index.tsx
    allow group posts to be created

--- src/pages/groups/[group-id]/index.tsx
+++ src/pages/groups/[group-id]/index.tsx
@@ -2,13 +2,17 @@ import PostEditor from "@/components/Editor/PostEditor";
 import Navbar from "@/components/Navbar/Navbar";
 import { PostEditorExtensions } from "@/helpers/global_vars";
 import useUser from "@/hooks/useUser";
+import useGroupPosts from "@/hooks/useGroupPosts";
 import GroupLayout from "@/layout/GroupLayout";
+import { getErrorMessage } from "@/lib/error-message";
 import { prisma } from "@/lib/prisma-client";
 import { Button } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
 import { Prisma } from "@prisma/client";
 import Placeholder from "@tiptap/extension-placeholder";
 import { useEditor } from "@tiptap/react";
 import { GetServerSideProps } from "next";
+import { useState } from "react";
 
 interface GroupPageProps {
 	data: GroupData | null;
@@ -22,6 +26,54 @@ export default function GroupPage(props: GroupPageProps) {
 			Placeholder.configure({ placeholder: "Share your thoughts" }),
 		],
 	});
+	const { createGroupPosts } = useGroupPosts(props.data.id, {
+		where: {
+			isDeleted: false,
+		},
+		orderBy: {
+			createdAt: "desc",
+		},
+		take: 10,
+	});
+	const [isSubmittingPost, setIsSubmittingPost] = useState(false);
+
+	async function handlePostSubmit() {
+		if (editor === null) {
+			notifications.show({
+				color: "red",
+				title: "Cannot get post content",
+				message: "Editor is not initialized",
+			});
+			return;
+		}
+
+		const editorNotEmpty =
+			!editor.isEmpty && editor.getText().trim().length !== 0;
+
+		if (editorNotEmpty) {
+			setIsSubmittingPost(true);
+			// prevent user from typing in the editor while submitting a post
+			editor.setEditable(false);
+			try {
+				await createGroupPosts(editor.getJSON());
+				editor.commands.clearContent();
+			} catch (error) {
+				notifications.show({
+					color: "red",
+					title: "Post submission failed",
+					message: getErrorMessage(error),
+				});
+			}
+			setIsSubmittingPost(false);
+			editor.setEditable(true);
+		} else {
+			notifications.show({
+				color: "orange",
+				title: "Post submission failed",
+				message: "Post content cannot be empty or just whitespace",
+			});
+		}
+	}
 
 	const currentUserIsAdmin = user?.id === props.data?.admin.id;
 	return (
@@ -34,7 +86,12 @@ export default function GroupPage(props: GroupPageProps) {
 				<div className="mx-auto w-full max-w-4xl">
 					<div className="flex flex-col gap-2">
 						<PostEditor editor={editor} />
-						<Button color="gray" size="md" className="w-full">
+						<Button
+							color="gray"
+							size="md"
+							className="w-full"
+							onClick={handlePostSubmit}
+							disabled={isSubmittingPost}>
 							Create post
 						</Button>
 					</div>
