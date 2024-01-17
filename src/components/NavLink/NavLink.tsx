56237822dcbe1e5db284a93a40efe4c73239ea61
Chisom Hermes Chigoziri <hermeschigoziri@gmail.com>
2024-01-17 21:19:45 +0100

    styling and code formatting

--- src/components/NavLink/NavLink.tsx
+++ src/components/NavLink/NavLink.tsx
@@ -2,39 +2,43 @@ import { Tooltip } from "@mantine/core";
 import Link from "next/link";
 
 export interface NavLinkProps {
-	href: string;
-	disabled: boolean;
-	active: boolean;
-	children: React.ReactNode;
+    href: string;
+    disabled: boolean;
+    active: boolean;
+    children: React.ReactNode;
 }
 
 /**
  * React component representing a navigation link
  */
 export default function NavLink(props: NavLinkProps) {
-	const { href, disabled, active, children, ...otherProps } = props;
+    const { href, disabled, active, children, ...otherProps } = props;
 
-	return (
-		<>
-			{disabled && (
-				<Tooltip.Floating label="You need an account!">
-					<div
-						className="flex-1 flex flex-col items-center gap-1 p-3 bg-primary-color-2 text-gray-500 rounded-md"
-						{...otherProps}>
-						{children}
-					</div>
-				</Tooltip.Floating>
-			)}
-			{!disabled && (
-				<Link
-					className={`flex flex-col flex-1 items-center gap-1 p-3 bg-primary-color-2 text-accent-color-2-l rounded-md hover:text-white ${
-						active ? "text-white" : ""
-					}`}
-					{...otherProps}
-					href={href}>
-					{children}
-				</Link>
-			)}
-		</>
-	);
+    const defaultStyles =
+        "flex flex-col flex-1 items-center gap-1 p-3 bg-primary-color-2 rounded-md basis-48 shrink-0";
+    return (
+        <>
+            {disabled && (
+                <Tooltip.Floating label="You need an account!">
+                    <div
+                        className={`${defaultStyles} text-gray-500`}
+                        {...otherProps}
+                    >
+                        {children}
+                    </div>
+                </Tooltip.Floating>
+            )}
+            {!disabled && (
+                <Link
+                    className={`${defaultStyles} rounded-md text-accent-color-2-l hover:text-white ${
+                        active ? "text-white" : ""
+                    }`}
+                    {...otherProps}
+                    href={href}
+                >
+                    {children}
+                </Link>
+            )}
+        </>
+    );
 }
