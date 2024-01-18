c5efab0cda4daa2b4245ab53dbb240830a17c6e8
Chisom Hermes Chigoziri <hermeschigoziri@gmail.com>
2024-01-18 09:49:00 +0100

    code formatting

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,9 +3,9 @@ import ProfileMenu from "../ProfileMenu/ProfileMenu";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/router";
 import {
-	IconUsersGroup,
-	IconUsersPlus,
-	IconListDetails,
+    IconUsersGroup,
+    IconUsersPlus,
+    IconListDetails,
 } from "@tabler/icons-react";
 import { notifications } from "@mantine/notifications";
 import NavLink from "../NavLink/NavLink";
@@ -14,86 +14,102 @@ import { Loader } from "@mantine/core";
 import styles from "./navbar.module.scss";
 
 interface NavbarProps {
-	toIndex?: boolean;
+    toIndex?: boolean;
 }
 
 /**
  * React component representing the Navigation bar of the site
  */
 export default function Navbar({ toIndex }: NavbarProps) {
-	const router = useRouter();
-	const { user, status, mutate } = useUser();
+    const router = useRouter();
+    const { user, status, mutate } = useUser();
 
-	function routeToProfile() {
-		router.push(`/users/${user?.accountName}`);
-	}
+    function routeToProfile() {
+        router.push(`/users/${user?.accountName}`);
+    }
 
-	async function handleSignout() {
-		try {
-			const { url } = await signOut({ redirect: false, callbackUrl: "/" });
-			// revalidate profile data
-			mutate();
-			router.push(url);
-		} catch (error: any) {
-			notifications.show({
-				color: "red",
-				title: "Signout Failed",
-				message: error.message,
-			});
-		}
-	}
+    async function handleSignout() {
+        try {
+            const { url } = await signOut({
+                redirect: false,
+                callbackUrl: "/",
+            });
+            // revalidate profile data
+            mutate();
+            router.push(url);
+        } catch (error: any) {
+            notifications.show({
+                color: "red",
+                title: "Signout Failed",
+                message: error.message,
+            });
+        }
+    }
 
-	const gettingUser = status === "GETTING_USER";
-	const hasUser = status === "HAS_USER" && user;
-	const noUser = status === "NO_USER";
-	return (
-		<nav className="flex flex-col items-stretch gap-4">
-			<div className="flex justify-between items-center">
-				<h1 className="font-extrabold text-5xl">
-					<Link href="/">ANON</Link>
-				</h1>
-				{gettingUser && <Loader color="gray" variant="dots" />}
-				{hasUser && (
-					<ProfileMenu
-						displayName={user.displayName}
-						onProfileMenuItemClicked={routeToProfile}
-						onSignoutMenuItemClicked={handleSignout}
-					/>
-				)}
-				{noUser && (
-					<Link
-						href={toIndex ? "/" : "/sign-in"}
-						className="text-lg font-semibold hover:text-accent-color-1">
-						{toIndex ? "HOMEPAGE" : "SIGN IN"}
-					</Link>
-				)}
-			</div>
-			<div className={`flex gap-1.5 overflow-x-scroll ${styles.navlinks}`}>
-				<NavLink
-					href="/"
-					disabled={false}
-					active={router.pathname === "/" || router.pathname.includes("posts")}
-					data-cy="home-page-link">
-					<IconListDetails size={35} />
-					<span className="font-semibold text-inherit">Public Posts</span>
-				</NavLink>
-				<NavLink
-					href="/groups"
-					disabled={!user}
-					active={router.pathname.includes("groups")}
-					data-cy="groups-page-link">
-					<IconUsersGroup size={35} />
-					<span className="font-semibold text-inherit">Groups</span>
-				</NavLink>
-				<NavLink
-					href="/join-group"
-					disabled={!user}
-					active={router.pathname.includes("join-group")}
-					data-cy="join-group-page-link">
-					<IconUsersPlus size={35} />
-					<span className="font-semibold text-inherit">Join Group</span>
-				</NavLink>
-			</div>
-		</nav>
-	);
+    const gettingUser = status === "GETTING_USER";
+    const hasUser = status === "HAS_USER" && user;
+    const noUser = status === "NO_USER";
+    return (
+        <nav className="flex flex-col items-stretch gap-4">
+            <div className="flex items-center justify-between">
+                <h1 className="text-5xl font-extrabold">
+                    <Link href="/">ANON</Link>
+                </h1>
+                {gettingUser && <Loader color="gray" variant="dots" />}
+                {hasUser && (
+                    <ProfileMenu
+                        displayName={user.displayName}
+                        onProfileMenuItemClicked={routeToProfile}
+                        onSignoutMenuItemClicked={handleSignout}
+                    />
+                )}
+                {noUser && (
+                    <Link
+                        href={toIndex ? "/" : "/sign-in"}
+                        className="text-lg font-semibold hover:text-accent-color-1"
+                    >
+                        {toIndex ? "HOMEPAGE" : "SIGN IN"}
+                    </Link>
+                )}
+            </div>
+            <div
+                className={`flex gap-1.5 overflow-x-scroll ${styles.navlinks}`}
+            >
+                <NavLink
+                    href="/"
+                    disabled={false}
+                    active={
+                        router.pathname === "/" ||
+                        router.pathname.includes("posts")
+                    }
+                    data-cy="home-page-link"
+                >
+                    <IconListDetails size={35} />
+                    <span className="font-semibold text-inherit">
+                        Public Posts
+                    </span>
+                </NavLink>
+                <NavLink
+                    href="/groups"
+                    disabled={!user}
+                    active={router.pathname.includes("groups")}
+                    data-cy="groups-page-link"
+                >
+                    <IconUsersGroup size={35} />
+                    <span className="font-semibold text-inherit">Groups</span>
+                </NavLink>
+                <NavLink
+                    href="/join-group"
+                    disabled={!user}
+                    active={router.pathname.includes("join-group")}
+                    data-cy="join-group-page-link"
+                >
+                    <IconUsersPlus size={35} />
+                    <span className="font-semibold text-inherit">
+                        Join Group
+                    </span>
+                </NavLink>
+            </div>
+        </nav>
+    );
 }
